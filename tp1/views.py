0d9bb182e6bd4713f28bf4406b26039671d69eaf
AssoulYasser <assoulyasser01@gmail.com>
2023-10-24 01:16:24 +0100

    decrypting message

--- tp1/views.py
+++ tp1/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .encryptions import ENCRYPTION_TYPES
 
 # Create your views here.
+
+@api_view(['GET'])
+def decrypt(request):
+    message = request.data['message']
+    encryption_type = str(request.data['encryption_type'])
+    encryption_value = request.data['encryption_value']
+
+    return Response(
+        status=200,
+        data={
+            "message":ENCRYPTION_TYPES[encryption_type]['decryption'](message, encryption_value)
+        }
+    )
