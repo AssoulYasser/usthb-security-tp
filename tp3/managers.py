ed7ec74f6f5bbe3466753d0260faaa5b853af6ab
AssoulYasser <assoulyasser01@gmail.com>
2023-11-18 07:30:41 +0100

    fixing user manager issues

--- tp3/managers.py
+++ tp3/managers.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.base_user import BaseUserManager
-from . import models
 
 class MyUserManage(BaseUserManager):
-    def user_condition_satisfied(**extra_fields):
-        if models.MyUser.REQUIRED_FIELDS not in extra_fields:
-            return Exception('FIELD ARE NOT SATISFIED')    
+    def user_condition_satisfied(self, **extra_fields):
+        from . import models
+        for field in models.MyUser.REQUIRED_FIELDS:
+            if field not in extra_fields:
+                return Exception('FIELD ARE NOT SATISFIED')    
         return None
     
     def create_user(self, **extra_fields):
         try:
-            self.user_condition_satisfied(extra_fields)
+            self.user_condition_satisfied(**extra_fields)
 
             email = self.normalize_email(extra_fields['email'])
             password = extra_fields['password']
@@ -26,5 +27,14 @@ class MyUserManage(BaseUserManager):
         except Exception as E:
             raise E
 
-    def create_superuser():
-        pass
+    def create_superuser(self, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(**extra_fields)
+
--- tp3/models.py
+++ tp3/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from . import managers
 
 class MyUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(primary_key=True)
     password = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
     
+    objects = managers.MyUserManage()
+
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = ['password']
