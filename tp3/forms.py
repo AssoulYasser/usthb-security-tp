818a32f47ebdfa77516ae90df4c02cceedcfa181
AssoulYasser <assoulyasser01@gmail.com>
2023-12-13 15:00:03 +0100

    deleting mac address and rsa keys from database schema

--- tp3/forms.py
+++ tp3/forms.py
@@ -4,15 +4,12 @@ from .models import MyUser
 class MyUserForm(forms.ModelForm):
     class Meta:
         model = MyUser
-        fields = ['first_name', 'last_name', 'mac_address', 'personal_image', 'email', 'password']
+        fields = ['first_name', 'last_name', 'personal_image', 'email', 'password']
         widgets = {
             'first_name': forms.TextInput(attrs={
                 'class': 'add-user-form-input'
             }),
             'last_name': forms.TextInput(attrs={
-                'class': 'add-user-form-input'
-            }),
-            'mac_address': forms.TextInput(attrs={
                 'class': 'add-user-form-input'
             }),
             'email': forms.EmailInput(attrs={
